b5a42c72be4395e6079fd8033af0a4bc8db481d6
ryuk <acaucans@gmail.com>
2020-05-16 11:50:07 +0900

    グラフのアニメーション設定

--- app/src/main/java/jp/ryuk/deglog/ui/chart/ChartFragment.kt
+++ app/src/main/java/jp/ryuk/deglog/ui/chart/ChartFragment.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
+import jp.ryuk.deglog.R
 import jp.ryuk.deglog.databinding.FragmentChartBinding
 
 
@@ -18,9 +19,45 @@ class ChartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentChartBinding.inflate(inflater, container, false)
-        (activity as AppCompatActivity).setSupportActionBar(binding.appBarChart)
 
+        binding.chartFilterOpen.setOnClickListener {
+            showFilterAnimation()
+        }
+
+        binding.chartFilterBg.setOnClickListener {
+            showFilterAnimation()
+        }
 
         return binding.root
     }
+
+    private fun showFilterAnimation() {
+        val container = binding.chartFilterContainer
+        val bg = binding.chartFilterBg
+        val icon = binding.chartFilterIcon
+        val rotate = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_right)
+
+        if (binding.chartFilterContainer.visibility == View.GONE) {
+            val slideIn = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_in_top)
+            val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+
+            container.startAnimation(slideIn)
+            container.visibility = View.VISIBLE
+            bg.startAnimation(fadeIn)
+            bg.visibility = View.VISIBLE
+            icon.startAnimation(rotate)
+            icon.setImageResource(R.drawable.ic_less)
+        } else {
+            val slideOut = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_out_top)
+            val fadeOut = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_out)
+
+            container.startAnimation(slideOut)
+            container.visibility = View.GONE
+            bg.startAnimation(fadeOut)
+            bg.visibility = View.GONE
+            icon.startAnimation(rotate)
+            icon.setImageResource(R.drawable.ic_more)
+        }
+    }
+
 }
